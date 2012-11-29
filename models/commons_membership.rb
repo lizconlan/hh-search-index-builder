08654d7611e7dbc86c0d00d6cdb3c303a67a6b64
lizconlan <lizconlan@gmail.com>
2012-11-29 21:39:46 +0000

    gah, missed one

--- models/commons_membership.rb
+++ models/commons_membership.rb
@@ -1,3 +1,5 @@
+incompatible character encodings: ASCII-8BIT and UTF-8
+
 class CommonsMembership < ActiveRecord::Base
   belongs_to :person
   has_many :commons_contributions, :class_name => "Contribution"
